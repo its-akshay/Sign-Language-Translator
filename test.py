458744963c0d3468da0e5929d190b86ddf543ea6
its-akshay <71098450+its-akshay@users.noreply.github.com>
2022-11-10 19:03:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def func():
     folder = "data/C"
     counter = 0
 
-    labels = ["A", "B", "C"]
+    labels = ["A", "ACROPOLIS", "B", "C", "HELLO", "NO", "OKAY", "YES"]
 
     while True:
         success, img = cap.read()
@@ -42,6 +42,10 @@ def func():
                 imgResizeShape = imgResize.shape
                 wGap = math.ceil((imgSize - wCal) / 2)
                 imgWhite[:, wGap:wCal + wGap] = imgResize
+                #below gives us the prediction and index
+                #using index get the value from the lables (0-based index)
+                #prediction for wGap images
+                #draw=false because in test file we dont want to display the 21points
                 prediction, index = classifier.getPrediction(imgWhite, draw=False)
                 print(prediction, index)
 
@@ -57,11 +61,13 @@ def func():
                 imgResizeShape = imgResize.shape
                 hGap = math.ceil((imgSize - hCal) / 2)
                 imgWhite[hGap:hCal + hGap, :] = imgResize
+                #same prediction for hGap Images
                 prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
             cv2.rectangle(imgOutput, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50),
-                          (255, 0, 255),
+                          (62, 50, 168),
                           cv2.FILLED)
+            #method used to draw a text on any image
             cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
             cv2.rectangle(imgOutput, (x - offset, y - offset),
                           (x + w + offset, y + h + offset), (255, 0, 255), 4)
@@ -73,73 +79,13 @@ def func():
         cv2.waitKey(1)
 
 
-def func2():
-    cap = cv2.VideoCapture(0)
-    detector = HandDetector(maxHands=1)
-
-    offset = 20
-    imgSize = 300
-
-    folder = "data/C"
-    counter = 0
-
-    while True:
-        success, img = cap.read()
-        hands, img = detector.findHands(img)
-        if hands:
-            hand = hands[0]
-            x, y, w, h = hand['bbox']
-
-            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
-
-            imgCropShape = imgCrop.shape
-
-            aspectRatio = h / w
-            # below for fixing height
-            if aspectRatio > 1:
-                k = imgSize / h
-                # calculated width
-                # if decimal then ceil(always higher)
-                wCal = math.ceil(k * w)
-
-                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-                imgResizeShape = imgResize.shape
-                wGap = math.ceil((imgSize - wCal) / 2)
-                imgWhite[:, wGap:wCal + wGap] = imgResize
-            # below for fixing width
-            else:
-                k = imgSize / w
-                hCal = math.ceil(k * h)
-                if (type(imgResize) == type(None)):
-                    pass
-                else:
-                    imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-                # imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-                imgResizeShape = imgResize.shape
-                hGap = math.ceil((imgSize - hCal) / 2)
-                imgWhite[hGap:hCal + hGap, :] = imgResize
-
-            cv2.imshow("ImageCrop", imgCrop)
-            cv2.imshow("ImageWhite", imgWhite)
-
-        cv2.imshow("Image", img)
-        key = cv2.waitKey(1)
-        if key == ord("s"):
-            counter += 1
-            cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-            print(counter)
-
-
-
 while 1:
-    image = 'signlang.jpg'
-    msg = 'HEARING IMPAIRMENT ASSISTANT'
-    choices = ['click to start', 'Data_collection/close', 'All done']
+    image = 'signlang1.png'
+    msg = '                   MINOR PROJECT FOR SIGN LANGAUGE TRANSLATOR'
+    choices = ['click to start', 'All done']
     reply = buttonbox(msg, image=image, choices=choices)
     if reply == choices[0]:
         func()
+
     if reply == choices[1]:
-        func2()
-    if reply == choices[2]:
         quit()
