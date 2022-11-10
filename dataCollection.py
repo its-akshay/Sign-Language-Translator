a0c8e8833789e7baa4c0cb82ca3322e019c08923
its-akshay <71098450+its-akshay@users.noreply.github.com>
2022-11-10 19:02:46 +0530

    Update dataCollection.py

--- dataCollection.py
+++ dataCollection.py
@@ -1,16 +1,18 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+#for a matrix we create when the image is not square
 import math
 import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 
+#offset will make it little big bigger while tracking
 offset = 20
 imgSize = 300
 
-folder = "data/C"
+folder = "data/ACROPOLIS"
 counter = 0
 
 while True:
@@ -19,8 +21,9 @@ while True:
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
-
+        #uint - unsigned int
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        #255 for having color image
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
         imgCropShape = imgCrop.shape
@@ -32,11 +35,20 @@ while True:
             # calculated width
             # if decimal then ceil(always higher)
             wCal = math.ceil(k * w)
-
+            #calculated width (by changing the image dimension parameter)
+            #at that point we have both height and calculated width
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            #resizing the image to very specific value that is calculated idth and image size
+
             imgResizeShape = imgResize.shape
+
+            #challange->making image centric
+            #imgsize->300 wCsl->150 then wGap = 75 and 75
+            #we are left with the both left and right
             wGap = math.ceil((imgSize - wCal) / 2)
+            #pushing forward the image by wGap
             imgWhite[:, wGap:wCal + wGap] = imgResize
+
         # below for fixing width
         else:
             k = imgSize / w
